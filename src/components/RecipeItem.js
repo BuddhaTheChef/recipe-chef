bbc6ec55521d164469734cbe7a9c024e969ea3f2
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-08-10 14:10:57 -0500

    recipe layout

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class RecipeItem extends Component {
   render() {
-    let { recipe } = this.props;
-   return (
-     <div>
-       <a href={recipe.href}>
-         <h4>{recipe.title}</h4>
-       </a>
-       <p>{recipe.ingredients}</p>
-     </div>
-   )
- }
+    let {recipe} = this.props;
+    return (<div className="recipe-item">
+      <div className="recipe-title">
+        <a href={recipe.href}>
+          <h4>{recipe.title}</h4>
+        </a>
+        <p>{recipe.ingredients}</p>
+      </div>
+      <img className="recipe-image" src={recipe.thumbnail} alt={recipe.title}/>
+    </div>)
+  }
 }
 
 export default RecipeItem;
